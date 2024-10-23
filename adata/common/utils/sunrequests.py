62a8a80e3eb33ea49d878a243d0c95d106271767
hangeve <hangever147@gmail.com>
2024-10-23 13:03:44 +0800

    增加socket5 代理

--- adata/common/utils/sunrequests.py
+++ adata/common/utils/sunrequests.py
@@ -12,7 +12,10 @@ import threading
 import time
 
 import requests
-
+import socks
+import socket
+from requests.exceptions import RequestException
+from urllib3.exceptions import NewConnectionError
 
 class SunProxy(object):
     _data = {}
@@ -49,45 +52,59 @@ class SunRequests(object):
     def request(self, method='get', url=None, times=3, retry_wait_time=1588, proxies=None, wait_time=None, **kwargs):
         """
         简单封装的请求，参考requests，增加循环次数和次数之间的等待时间
-        :param proxies: 代理配置
+        :param proxies: 代理配置，支持 HTTP 和 SOCKS5
         :param method: 请求方法： get；post
-        :param url: url
-        :param times: 次数，int
-        :param retry_wait_time: 重试等待时间，毫秒
-        :param wait_time: 等待时间：毫秒；表示每个请求的间隔时间，在请求之前等待sleep，主要用于防止请求太频繁的限制。
-        :param kwargs: 其它 requests 参数，用法相同
+        :param url: 请求的 URL
+        :param times: 重试次数，默认 3 次
+        :param retry_wait_time: 重试之间的等待时间，毫秒
+        :param wait_time: 请求之间的等待时间，毫秒
+        :param kwargs: 其它 requests 参数
         :return: res
         """
-        # 1. 获取设置代理
+        # 1. 获取代理配置
         proxies = self.__get_proxies(proxies)
-        # 2. 请求数据结果
+
+        # 2. 执行请求
         res = None
         for i in range(times):
             if wait_time:
-                time.sleep(wait_time / 1000)
-            res = requests.request(method=method, url=url, proxies=proxies, **kwargs)
-            if res.status_code in (200, 404):
-                return res
-            time.sleep(retry_wait_time / 1000)
-            if i == times - 1:
-                return res
+                time.sleep(wait_time / 1000)  # 请求之间的等待
+
+            try:
+                res = requests.request(method=method, url=url, proxies=proxies, **kwargs)
+                if res.status_code in (200, 404):
+                    return res
+            except (RequestException, NewConnectionError) as e:
+                print(f"Request failed: {e}")
+
+            time.sleep(retry_wait_time / 1000)  # 重试之间的等待时间
+
         return res
 
     def __get_proxies(self, proxies):
         """
-        获取代理配置
+        获取代理配置，支持 HTTP 和 SOCKS5
         """
         if proxies is None:
             proxies = {}
+
         is_proxy = SunProxy.get('is_proxy')
         ip = SunProxy.get('ip')
         proxy_url = SunProxy.get('proxy_url')
-        if not ip and is_proxy and proxy_url:
-            ip = requests.get(url=proxy_url).text.replace('\r\n', '') \
-                .replace('\r', '').replace('\n', '').replace('\t', '')
-        if is_proxy and ip:
-            proxies = {'https': f"http://{ip}", 'http': f"http://{ip}"}
+        proxy_type = SunProxy.get('proxy_type', 'http')  # 默认 HTTP 代理，如果未设置类型
+        socks5_proxy = SunProxy.get('socks5_proxy')
+
+        # 如果是 SOCKS5 代理
+        if proxy_type == 'socks5' and socks5_proxy:
+            proxies = {
+                'http': f'{socks5_proxy}',
+                'https': f'{socks5_proxy}',
+            }
+        # 如果是 HTTP/HTTPS 代理
+        elif not ip and is_proxy and proxy_url:
+            ip = requests.get(url=proxy_url).text.strip()
+            if is_proxy and ip:
+                proxies = {'https': f"http://{ip}", 'http': f"http://{ip}"}
+
         return proxies
-
-
 sun_requests = SunRequests()
